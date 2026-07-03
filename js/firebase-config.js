a60c4921ca1bc4e1544788fb1d7770fb0e413237
Fmsait <fmsaith@yahoo.com.br>
2026-07-03 17:03:17 -0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCPbMtmfDj4H8N-XnpmJeTF7i2HzYSL6aM",
-  authDomain: "filosofia-interativa.firebaseapp.com",
-  projectId: "filosofia-interativa",
-  storageBucket: "filosofia-interativa.firebasestorage.app",
-  messagingSenderId: "856293312942",
-  appId: "1:856293312942:web:80e8e778aee1ba9711e03f"
+// Substitua os campos abaixo pelos dados do seu projeto no Firebase Console.
+// Firebase Console > Configurações do projeto > Seus apps > App Web.
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI_SUA_API_KEY",
+  authDomain: "COLE_AQUI.firebaseapp.com",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI.appspot.com",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const GAME_ID_HERACLITO = "heraclito_logos_movimento";
