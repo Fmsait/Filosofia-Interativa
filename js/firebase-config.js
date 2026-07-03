0afd0a803e20ae8897ae3053876a85d90551c7dc
Fmsait <fmsaith@yahoo.com.br>
2026-07-03 16:24:04 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // Substitua os campos abaixo pelos dados do seu projeto no Firebase Console.
 // Firebase Console > Configurações do projeto > Seus apps > App Web.
 export const firebaseConfig = {
-  apiKey: "COLE_AQUI_SUA_API_KEY",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyCPbMtmfDj4H8N-XnpmJeTF7i2HzYSL6aM",
+  authDomain: "filosofia-interativa.firebaseapp.com",
+  projectId: "filosofia-interativa",
+  storageBucket: "filosofia-interativa.firebasestorage.app",
+  messagingSenderId: "856293312942",
+  appId: "1:856293312942:web:80e8e778aee1ba9711e03f"
 };
 
 export const GAME_ID_HERACLITO = "heraclito_logos_movimento";
