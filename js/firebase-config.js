8e5d8d2b852cd97eb412aabffcc80029baae149b
Fmsait <fmsaith@yahoo.com.br>
2026-07-03 17:19:13 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCPbMtmfDj4H8N-XnpmJeTF7i2HzYSL6aM",
   authDomain: "filosofia-interativa.firebaseapp.com",
   projectId: "filosofia-interativa",
@@ -13,5 +7,4 @@ const firebaseConfig = {
   appId: "1:856293312942:web:80e8e778aee1ba9711e03f"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const GAME_ID_HERACLITO = "heraclito_logos_movimento";
